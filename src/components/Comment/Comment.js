12496b3ca20276609c8bff052a46320855e9f75c
cihadacar <cihadacar1@hotmail.com>
2023-10-01 19:18:45 +0300

    comments and like features implementation

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,33 @@
+import { Avatar, CardContent, InputAdornment, OutlinedInput } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom";
+import { lime } from '@mui/material/colors';
+
+function Comment(props) {
+    const { text, userId, userName } = props;
+
+    return (
+        <CardContent>
+            <OutlinedInput
+                size="small"
+                readOnly="true"
+                id="outlined-adornment-amount"
+                multiline
+                inputProps={{ maxLength: 250 }}
+                fullWidth
+                value={text}
+                startAdornment={
+                    <InputAdornment position="start">
+                        <Link style={{ textDecoration: "none" }} to={{ pathname: '/users/' + userId }}>
+                            <Avatar sx={{ width: 27, height: 27, bgcolor: lime[900] }} aria-label="recipe">
+                                {userName.charAt(0).toUpperCase()}
+                            </Avatar>
+                        </Link>
+                    </InputAdornment>
+                }>
+            </OutlinedInput>
+        </CardContent>
+    )
+}
+
+export default Comment;
